427966a77de54773e9aaeda400c0a6e3401119c7
Sandesh Pathak <sandeshpathak0112@gmail.com>
2017-09-13 11:54:48 -0400

    passport oauth progress

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 	res.send({hi: 'there'})
-})
+});
+
+console.log(keys.googleClientID,keys.googleClientSecret)
+
+passport.use(
+	new GoogleStrategy(
+	{
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback'
+	}, 
+	accessToken => {
+		console.log(accessToken);
+	})
+);
+
+app.get('/auth/google', 
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('auth/google/callback',
+	passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 5000
 
