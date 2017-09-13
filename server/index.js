9d5f106eba8d143fc18df3759f8c8f8d1a53b0f5
Sandesh Pathak <sandeshpathak0112@gmail.com>
2017-09-13 12:39:56 -0400

    directoru changed

--- server/index.js
+++ server/index.js
@@ -17,8 +17,11 @@ passport.use(
 		clientSecret: keys.googleClientSecret,
 		callbackURL: '/auth/google/callback'
 	}, 
-	accessToken => {
-		console.log(accessToken);
+	(accessToken, refreshToken, profile, done) => {
+		console.log('accessToken',accessToken);
+		console.log('refreshToken',refreshToken);
+		console.log('profile',profile);
+		console.log('done',done);
 	})
 );
 
@@ -28,9 +31,7 @@ app.get('/auth/google',
 	})
 );
 
-app.get('auth/google/callback',
-	passport.authenticate('google')
-);
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000
 
